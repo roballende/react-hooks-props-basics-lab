f1ec89b4d0d3d75e046fe681f7b6d289e7e092a8
Robert Allende <allende.rob@gmail.com>
2022-12-15 16:33:56 -0600

    commit tree 22905c3b62651ba5295f08fa7217ca03d052a807

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,32 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  if(!props.bio){
+    return (
+      <div id="about">
+      <h2>About Me</h2>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {<Links
+        textContentGit={user.links.github}
+        textContentLi={user.links.linkedin}
+      />}
+    </div>
+    );
+  } else {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {<Links
+        textContentGit={user.links.github}
+        textContentLi={user.links.linkedin}
+      />}
     </div>
   );
+  }
 }
 
 export default About;
---
+++ src/components/Links.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Links ({textContentGit, textContentLi}) {
+    return (
+       <div>
+            <h3>Links</h3>
+            <a href={textContentGit}>https://github.com/liza</a>
+            <a href={textContentLi}>https://www.linkedin.com/in/liza/</a>
+       </div> 
+    )
+}
+
+
+
+
+
+
+
+export default Links;
